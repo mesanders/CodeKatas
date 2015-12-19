0e54ef758dd26c8ff01bcbd357ecedfec96139d0
Michael Sanders <mesanders@mail.bradley.edu>
2015-12-19 04:16:07 -0600

    Added new kata area for Algorithms. This will be used to test various Algorithms, or go through some basic algorithms that I need to remember.

---
+++ Algorithms/src/test/java/kata/algorithms/AlgorithmsCustomTestRunner.java
@@ -0,0 +1,22 @@
+package kata.algorithms;
+
+public class AlgorithmsCustomTestRunner {
+public static final String ANSI_RESET = "\u001B[0m";
+public static final String ANSI_BLACK = "\u001B[1;30m";
+public static final String ANSI_RED = "\u001B[1;31m";
+public static final String ANSI_GREEN = "\u001B[1;32m";
+public static final String ANSI_YELLOW = "\u001B[1;33m";
+public static final String ANSI_BLUE = "\u001B[1;34m";
+public static final String ANSI_PURPLE = "\u001B[1;35m";
+public static final String ANSI_CYAN = "\u001B[1;36m";
+public static final String ANSI_WHITE = "\u001B[1;37m";
+
+	public static void main(String[] args) {
+		echo("HELLO TEST");
+	}
+
+	public static void echo(String message) {
+		System.out.println(ANSI_WHITE + "Running: \t" + message + ANSI_RESET);		
+	}
+
+}
